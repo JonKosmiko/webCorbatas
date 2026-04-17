7f43fdef7f09143c4c17ff7b664c94b073a8022c
JonKosmiko <uresberuetadelavega@gmail.com>
2026-04-17 17:37:38 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,96 @@
+const CACHE_NAME = 'webCorbatas_v7';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/estilos.css',
+  './img/01.jpg',
+  './img/02.jpg',
+  './img/03.jpg',
+  './img/04.jpg',
+  './img/05.jpg',
+  './img/06.jpg',
+  './img/07.jpg',
+  './img/08.jpg',
+  './img/09.jpg',
+  './img/10.jpg',
+  './img/inicio_01.jpg',
+  './img/inicio_dobleA.jpg',
+  './img/inicio_dobleB.jpg',
+  './img/inicio_final.jpg',
+  './img/inicio_invitados.jpg',
+  './img/inicio_novio.jpg',
+  './img/inicio_portada.jpg',
+  './img/inicio_seccion_americana.jpg',
+  './img/inicio_seccion_camisa.jpg',
+  './img/inicio_seccion_chaleco.jpg',
+  './img/inicio_seccion_corbatas.jpg',
+  './img/inicio_seccion_pantalon.jpg',
+  './img/inicio_seccion_zapato.jpg',
+  './fonts/PlayfairDisplay-Black.ttf',
+  './fonts/PlayfairDisplay-Bold.ttf',
+  './fonts/PlayfairDisplay-Italic.ttf',
+  './fonts/PlayfairDisplay-Regular.ttf',
+];
+
+// 1. INSTALACIÓN: Guardamos todo lo básico
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caché abierta y assets guardados');
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 2. ACTIVACIÓN: Limpieza de cachés antiguas
+self.addEventListener('activate', (event) => {
+  self.clients.claim();
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// 3. FETCH: Estrategia híbrida
+self.addEventListener('fetch', (event) => {
+  
+  // A. Si el usuario navega (va a la home o refresca)
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match('./index.html');
+      })
+    );
+    return;
+  }
+
+  // B. Para el resto de assets (imágenes, CSS, fuentes)
+  // Usamos Stale-While-Revalidate: Sirve rápido de caché, actualiza de red.
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        
+        const fetchPromise = fetch(event.request).then((networkResponse) => {
+          // Si la respuesta es válida, guardamos copia en caché
+          if (networkResponse && networkResponse.status === 200) {
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
+        }).catch(() => {
+          // Si no hay red, no pasa nada, ya estamos intentando servir de caché
+        });
+
+        // Devolvemos la respuesta de caché si existe, sino esperamos a la de red
+        return cachedResponse || fetchPromise;
+      });
+    })
+  );
+});
